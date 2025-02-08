0fc5080823e2df57dc1b076d594eab769a5a19c0
itamaradin15 <itamaradin15@gmail.com>
2025-02-08 10:20:59 -0500

    added headers

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,3 +1,13 @@
 export default function handler(req, res) {
-    res.status(200).json({ message: "Hola, esta es una API demo en Next.js!" });
+  res.setHeader("Access-Control-Allow-Origin", "https://brioperu.com"); // Permitir solo este dominio
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  if (req.method === "OPTIONS") {
+    res.status(200).end(); // Responder a pre-flight requests
+
+    return;
+  }
+
+  res.status(200).json({ message: "Hello from Vercel API" });
 }
